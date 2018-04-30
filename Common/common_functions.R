33fbe85a97f39eef72f80619f487fd4c6604356a
shevtsov200 <shevtsov200@gmail.com>
2018-04-30 14:16:46 +0300

    Extract validate_model function to common_functions.R file, move tic tac toe data set to Common directory.

---
+++ Common/common_functions.R
@@ -0,0 +1,42 @@
+validate_model <- function(classifier, read_data, n_seq) {
+  A_raw <- read_data()
+  rounded_digits <- 2
+  set_size <- dim(A_raw)[1]
+  
+  set.seed(12345)
+  A_rand <- A_raw[ order(runif(set_size)), ]
+  
+  seq_length <- length(n_seq)
+  
+  percentage_matrix <- sapply(n_seq, function(x) {
+    train_set_size <- round(set_size*x)
+    
+    index <- sample(1:set_size,train_set_size)
+    A_train <- A_rand[index, ]
+    A_test <- A_rand[-index, ]
+    prop.table(table(A_train$type))
+    prop.table(table(A_test$type))
+    
+    A_classifier <- classifier(type ~ ., data = A_train)
+    
+    A_predicted <- predict(A_classifier, A_test)
+    
+    t <- table(A_predicted, A_test$type)
+    
+    rownames(t) <- c("predicted_negative","predicted_positive")
+    colnames(t) <- c("real_negative","real_positive")
+    
+    train_set_percentage <- round(train_set_size/(set_size/100))
+    
+    test_set_size <- set_size - train_set_size
+    test_set_percent <- (test_set_size/100)
+    
+    predicted_correctly <- round((t["predicted_positive","real_positive"]+t["predicted_negative","real_negative"])/test_set_percent, digits=rounded_digits)
+    predicted_wrong <- round((t["predicted_negative","real_positive"]+t["predicted_positive","real_negative"])/test_set_percent, digits=rounded_digits)
+    
+    return (c(train_set_percentage,predicted_correctly,predicted_wrong))
+  })
+  percentage_data_frame <- as.data.frame(t(percentage_matrix))
+  names(percentage_data_frame) <- c("train_set_percentage","predicted_correctly","predicted_wrong")
+  return(percentage_data_frame)
+}
--- Lab1_Bayees/lab1/lab1.R
+++ Lab1_Bayees/lab1/lab1.R
@@ -4,8 +4,12 @@ library(data.table)
 
 data(spam)
 
+common_dir_path <- "../../Common/"
+
+source(paste(common_dir_path,"common_functions.R",sep=""))
+
 read_tic_tac_toe <- function() {
-  raw_data <- read.table("Tic_tac_toe.txt", sep = ",", stringsAsFactors = TRUE)
+  raw_data <- read.table(paste(common_dir_path,"Tic_tac_toe.txt",sep=""), sep = ",", stringsAsFactors = TRUE)
   colnames(raw_data)[colnames(raw_data)=="V10"] <- "type"
   return(raw_data)
 }
@@ -33,49 +37,6 @@ generate_points <- function() {
   pairs(point_frame[1:2], main="scatter plot",
         pch=circle_pch, bg=point_frame$type)
   return (point_frame)
-}
-
-validate_model <- function(classifier, read_data, n_seq) {
-  A_raw <- read_data()
-  rounded_digits <- 2
-  set_size <- dim(A_raw)[1]
-  
-  set.seed(12345)
-  A_rand <- A_raw[ order(runif(set_size)), ]
-  
-  seq_length <- length(n_seq)#(seq_to - seq_from + seq_step)/seq_step
-  
-  percentage_matrix <- sapply(n_seq, function(x) {
-    train_set_size <- round(set_size*x)
-    
-    index <- sample(1:set_size,train_set_size)
-    A_train <- A_rand[index, ]
-    A_test <- A_rand[-index, ]
-    prop.table(table(A_train$type))
-    prop.table(table(A_test$type))
-    
-    A_classifier <- classifier(type ~ ., data = A_train)
-    
-    A_predicted <- predict(A_classifier, A_test)
-    
-    t <- table(A_predicted, A_test$type)
-    
-    rownames(t) <- c("predicted_negative","predicted_positive")
-    colnames(t) <- c("real_negative","real_positive")
-    
-    train_set_percentage <- round(train_set_size/(set_size/100))
-    
-    test_set_size <- set_size - train_set_size
-    test_set_percent <- (test_set_size/100)
-    
-    predicted_correctly <- round((t["predicted_positive","real_positive"]+t["predicted_negative","real_negative"])/test_set_percent, digits=rounded_digits)
-    predicted_wrong <- round((t["predicted_negative","real_positive"]+t["predicted_positive","real_negative"])/test_set_percent, digits=rounded_digits)
-    
-    return (c(train_set_percentage,predicted_correctly,predicted_wrong))
-  })
-  percentage_data_frame <- as.data.frame(t(percentage_matrix))
-  names(percentage_data_frame) <- c("train_set_percentage","predicted_correctly","predicted_wrong")
-  return(percentage_data_frame)
 }
 
 seq_step <- 0.05
